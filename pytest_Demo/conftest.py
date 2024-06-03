a06585995ec08f040d7e29293e307e87f05e4813
subbaram <subbaramt99@gmail.com>
2024-06-03 22:55:06 +0530

    pytest_fixture data loading and parameterization

--- pytest_Demo/conftest.py
+++ pytest_Demo/conftest.py
@@ -2,6 +2,8 @@
 # conftest will make generalized the for all test case
 # fixture make it available in all test case.
 # when we passing the fixture method name into test it will run as before/after test execute
+# Datadriven and paramertization can be done with return statement in tuple formate.
+# when we define fixture scope to class alone, it will run before class will initiated and at the end
 
 import pytest
 
